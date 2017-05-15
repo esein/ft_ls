f49d133069a479447fbec11d49c34f106a0bd1e5
Gaetan CADIOU <gcadiou@e3r3p16.42.fr>
2017-05-15 17:20:26 +0200

    change priority in tri : S override t

--- tri.c
+++ tri.c
@@ -6,7 +6,7 @@
 /*   By: gcadiou <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/21 20:06:29 by gcadiou           #+#    #+#             */
-/*   Updated: 2017/04/26 10:58:19 by gcadiou          ###   ########.fr       */
+/*   Updated: 2017/05/15 17:18:06 by gcadiou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -133,14 +133,14 @@ void		check_tri(struct s_infos *infos, struct s_lsopt *ls_opt)
 	if (ls_opt->f == 0)
 	{
 		tri_ascii(infos, ls_opt);
-		if (ls_opt->t > 0)
+		if (ls_opt->S > 0)
+			tri_size(infos, ls_opt);
+		else if (ls_opt->t > 0)
 		{
 			if (ls_opt->u > 0)
 				tri_atime(infos, ls_opt);
 			else
 				tri_mtime(infos, ls_opt);
 		}
-		else if (ls_opt->S > 0)
-			tri_size(infos, ls_opt);
 	}
 }
